03a23336801eb508615c2f4978a2fe07e974aeec
Pascal Precht <pascal.precht@googlemail.com>
2014-05-09 11:16:26 +0200

    ALL THE TESTS

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,27 +6,39 @@ angular.module('myApp.controllers', []).
     controller('AppCtrl', function ($scope, socket) {
         $scope.tweets = [
 
-          // {
-          //   text: 'Hello #codefrontio! We hope you enjoy the talk!',
-          //   user: {
-          //     profile_image_url: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
-          //     name: 'Pascal Precht ʕ•̫͡•ʔ'
-          //   }
-          // },
-          // {
-          //   text: 'FULL SPECTRUM TESTING ALL THE THINGS!',
-          //   user: {
-          //     profile_image_url: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
-          //     name: 'Pascal Precht ʕ•̫͡•ʔ'
-          //   }
-          // },
-          // {
-          //   text: 'YOLO',
-          //   user: {
-          //     profile_image_url: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
-          //     name: 'Pascal Precht ʕ•̫͡•ʔ'
-          //   }
-          // }
+          {
+            text: 'Hello #codefrontio! We hope you enjoy the talk!',
+            user: {
+              profile_image_url: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
+              name: 'Pascal Precht ʕ•̫͡•ʔ',
+              screen_name: 'PascalPrecht'
+            },
+            lang: 'en',
+            retweet_count: 0,
+            clicked: false
+          },
+          {
+            text: 'FULL SPECTRUM TESTING ALL THE THINGS!',
+            user: {
+              profile_image_url: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
+              name: 'Pascal Precht ʕ•̫͡•ʔ',
+              screen_name: 'PascalPrecht'
+            },
+            lang: 'en',
+            retweet_count: 0,
+            clicked: false
+          },
+          {
+            text: 'YOLO',
+            user: {
+              profile_image_url: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
+              name: 'Pascal Precht ʕ•̫͡•ʔ',
+              screen_name: 'PascalPrecht'
+            },
+            lang: 'en',
+            retweet_count: 0,
+            clicked: false
+          }
         ];
 
         socket.on('tweets', function (data) {
