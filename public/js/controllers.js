2caad396490f11b7455e3a860721b18a26c85532
Pascal Precht <pascal.precht@googlemail.com>
2014-05-09 09:12:28 +0200

    make name linkable

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -9,21 +9,21 @@ angular.module('myApp.controllers', []).
           // {
           //   text: 'Hello #codefrontio! We hope you enjoy the talk!',
           //   user: {
-          //     image: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
+          //     profile_image_url: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
           //     name: 'Pascal Precht ʕ•̫͡•ʔ'
           //   }
           // },
           // {
           //   text: 'FULL SPECTRUM TESTING ALL THE THINGS!',
           //   user: {
-          //     image: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
+          //     profile_image_url: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
           //     name: 'Pascal Precht ʕ•̫͡•ʔ'
           //   }
           // },
           // {
           //   text: 'YOLO',
           //   user: {
-          //     image: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
+          //     profile_image_url: 'http://pbs.twimg.com/profile_images/449147152036397057/u0bWmFuc_normal.jpeg',
           //     name: 'Pascal Precht ʕ•̫͡•ʔ'
           //   }
           // }
